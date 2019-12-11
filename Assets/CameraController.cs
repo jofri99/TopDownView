9e491eb753a89585eebf92e769602b44bedb5291
Joris Friedl <joofriii@googlemail.com>
2019-12-11 21:51:44 +0100

    Add basic player movement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,37 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(target.position);
+        Debug.Log(offset);
+        Vector3 desiredPosition =  target.position + offset;
+        Debug.Log(desiredPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(target.position);
+        //Debug.Log(offset);
+        Vector3 desiredPosition =  target.position + offset;
+        //Debug.Log(desiredPosition);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+
+    
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    private Rigidbody rb;
+    Vector3 moveDirection;
+    // Start is called before the first frame update
+
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();   
+    }
+
+    void FixedUpdate(){
+        Move();
+    }
+
+    void Move() {
+        rb.velocity = moveDirection * movementSpeed * Time.deltaTime;
+    }
+
+    void HandleMovementInput ()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        rb.velocity = Vector3.zero;
+    }
+}
